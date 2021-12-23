a1c038db991739da42eb83eb8538ab34b10038b1
vishal-bhosle <vishal@elitefit.ai>
2021-12-23 11:49:00 +0530

    WebView Camera issue updated & Logo added

--- app/src/main/java/ai/elitefit/myapplication/MainActivity.java
+++ app/src/main/java/ai/elitefit/myapplication/MainActivity.java
@@ -1,103 +1,218 @@
 package ai.elitefit.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
-import android.webkit.WebSettings;
+import android.view.View;
+import android.view.Window;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+import pub.devrel.easypermissions.EasyPermissions;
 
-    WebView web;
-    String url = "https://elitefitforyou.com";
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+
+    WebView webView;
+    String url = "https://elitefitforyou.com/";
+
+    private final String TAG = "TEST";
+    private PermissionRequest mPermissionRequest;
+
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+    private static final String[] PERM_CAMERA = {
+            Manifest.permission.CAMERA
+    };
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        Objects.requireNonNull(getSupportActionBar()).hide(); // hide the title bar
 
         setContentView(R.layout.activity_main);
 
-        web = findViewById(R.id.webView);
+        webView = findViewById(R.id.webView);
 
-        WebSettings webSettings = web.getSettings();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        webView.getSettings().setUserAgentString(System.getProperty("http.agent"));
 
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setDatabaseEnabled(true);
-        webSettings.setUserAgentString(System.getProperty("http.agent"));
+        webView.setWebChromeClient(new WebChromeClient() {
 
-        web.setWebViewClient(new Callback());
-
-        if (savedInstanceState == null)
-        {
-            web.loadUrl(url);
-        }
-    }
-
-    private class Callback extends WebViewClient {
-
-        @Override
-        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
-            return false;
-        }
-
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (url.startsWith("http") || url.startsWith("https")) {
-                return false;
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                getWindow().setTitle(title); //Set Activity tile to page title.
             }
-            if (url.startsWith("intent")) {
-                try {
-                    Intent intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
-                    String fallbackUrl = intent.getStringExtra("browser_fallback_url");
-                    if (fallbackUrl != null) {
-                        view.loadUrl(fallbackUrl);
-                        return true;
+
+            // Grant permissions for cam
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                Log.i(TAG, "onPermissionRequest");
+                mPermissionRequest = request;
+                final String[] requestedResources = request.getResources();
+                for (String r : requestedResources) {
+                    if (r.equals(PermissionRequest.RESOURCE_VIDEO_CAPTURE)) {
+                        // In this sample, we only accept video capture request.
+                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Allow Permission to camera")
+                                .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                        mPermissionRequest.grant(new String[]{PermissionRequest.RESOURCE_VIDEO_CAPTURE});
+                                        Log.d(TAG,"Granted");
+                                    }
+                                })
+                                .setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                        mPermissionRequest.deny();
+                                        Log.d(TAG,"Denied");
+                                    }
+                                });
+                        AlertDialog alertDialog = alertDialogBuilder.create();
+                        alertDialog.show();
+
+                        break;
                     }
-                } catch (URISyntaxException e) {
-                    // Syntax problem with uri
                 }
             }
-            if (url.startsWith("tel:")) {
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
-                startActivity(intent);
-            } else if (url.startsWith("mailto:")) {
-                Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse(url));
-                startActivity(intent);
+
+            @Override
+            public void onPermissionRequestCanceled(PermissionRequest request) {
+                super.onPermissionRequestCanceled(request);
+                Toast.makeText(MainActivity.this,"Permission Denied", Toast.LENGTH_SHORT).show();
             }
-            view.loadUrl(url);
-            return true;
+        });
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+                return false;
+            }
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("http") || url.startsWith("https")) {
+                    return false;
+                }
+                if (url.startsWith("intent")) {
+                    try {
+                        Intent intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
+                        String fallbackUrl = intent.getStringExtra("browser_fallback_url");
+                        if (fallbackUrl != null) {
+                            view.loadUrl(fallbackUrl);
+                            return true;
+                        }
+                    } catch (URISyntaxException e) {
+                        // Syntax problem with uri
+                    }
+                }
+                if (url.startsWith("tel:")) {
+                    Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
+                    startActivity(intent);
+                }
+                else if (url.startsWith("mailto:")) {
+                    Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse(url));
+                    startActivity(intent);
+                }
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                //hide loading image
+                findViewById(R.id.imageView).setVisibility(View.GONE);
+                //show webview
+                findViewById(R.id.webView).setVisibility(View.VISIBLE);
+            }
+
+        });
+
+        if (hasCameraPermission()) {
+            if (savedInstanceState == null)
+            {
+                webView.loadUrl(url);
+            }
         }
+        else {
+            EasyPermissions.requestPermissions(
+                    this,
+                    "This app needs access to your camera so you can take pictures.",
+                    REQUEST_CAMERA_PERMISSION,
+                    PERM_CAMERA);
+        }
+
+    }
+
+    private boolean hasCameraPermission() {
+        return EasyPermissions.hasPermissions(MainActivity.this, PERM_CAMERA);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+
     }
 
     @Override
     public void onBackPressed() {
-        if (web.canGoBack()) {
-            web.goBack();
+        if (webView.canGoBack()) {
+            webView.goBack();
         } else {
             super.onBackPressed();
         }
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState)
+    protected void onSaveInstanceState(@NonNull Bundle outState)
     {
         super.onSaveInstanceState(outState);
-        web.saveState(outState);
+        webView.saveState(outState);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState)
     {
         super.onRestoreInstanceState(savedInstanceState);
-        web.restoreState(savedInstanceState);
+        webView.restoreState(savedInstanceState);
     }
+
 }
